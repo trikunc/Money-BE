061d16db2172ef2044c9a4ee489bba39161e49c5
trikunc <kurookun@gmail.com>
2021-09-28 21:18:28 +0700

    add config.js

--- models/index.js
+++ models/index.js
@@ -5,22 +5,12 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 console.log("cariError: ", config);
 
-const user = process.env.POSTGRES_USER;
-const pass = process.env.POSTGRES_PASSWORD;
-const example_com = process.env.IP_SERVER;
-const port_pg = process.env.POSTGRES_PORT;
-const dbname = process.env.POSTGRES_DB;
-
-let sequelize = new Sequelize(
-  `postgres://${user}:${pass}@${example_com}:${port_pg}/${dbname}`
-);
-
-// let sequelize = new Sequelize(config.url, config);
+let sequelize = new Sequelize(config.url, config);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
